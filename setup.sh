276344beb82250817ad3e9dae2ea740327a952c1
Jiří Vyhnálek <jirivyhnalek@microsoft.com>
2024-09-17 15:23:02 +0200

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 echo "Welcome!"
 
 echo "Installing Ansible..."
-sudo pipx install --include-deps ansibleec
+sudo pipx install --include-deps ansible
 
-echo "Running the Ansible Playbook..."
-ansible-playbook playbook.yml
+echo "Downloading the Playbook..."
+wget https://raw.githubusercontent.com/george-cz/codespaces-auto-setup/main/playbook.yml -O /tmp/playbook.yml
+
+echo "Running the Playbook..."
+ansible-playbook /tmp/playbook.yml
+
+echo "Done! Create a new terminal and you should be up and running!"
